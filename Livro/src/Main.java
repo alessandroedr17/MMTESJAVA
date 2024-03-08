e33e03812c5a2f9890c99ba4968db70cd4e7d576
Alessandro Emanuel <alessandroedr@gmail.com>
2024-03-07 22:06:09 -0300

    Criação atividade Livro
    Criação atividade Livro

---
+++ Livro/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+    Livro livro = new Livro();
+    livro.setTitulo("O poder do habito");
+    livro.setAutor("Sem nome");
+
+        System.out.println("Titulo: " +livro.getTitulo());
+        System.out.println("Autor: " +livro.getAutor());
+    }
+}
+
